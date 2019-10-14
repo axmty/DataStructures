c00777617fcc306760abb4935c631900bcd364f7
Metgy Alexandre <alexandre.metgy@gmail.com>
2019-10-14 21:43:16 +0200

    Implement List<T> for SingleLinkedList<T>, instead of just IEnumerable<T>. Implement properties Count, IsReadOnly and indexer.

--- DataStructures/SingleLinkedList.cs
+++ DataStructures/SingleLinkedList.cs
@@ -2,34 +2,114 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace DataStructures
 {
-    public class SingleLinkedList<T> : IEnumerable<T>
+    public class SingleLinkedList<T> : IList<T>
     {
+        private SingleNode<T> _head { get; set; }
+
+        private SingleNode<T> _tail { get; set; }
+
         public SingleLinkedList()
         {
-            this.Head = null;
-            this.Tail = null;
+            _head = null;
+            _tail = null;
         }
 
-        private SingleNode<T> Head { get; set; }
+        public T this[int index]
+        {
+            get => this.ReachNode(index).Value;
+            set => this.ReachNode(index).Value = value;
+        }
 
-        private SingleNode<T> Tail { get; set; }
+        public int Count
+        {
+            get
+            {
+                var node = _head;
+                var count = 0;
 
-        public void Insert(T value, int index)
+                while (node != null)
+                {
+                    count++;
+                    node = node.Next;
+                }
+
+                return count;
+            }
+        }
+
+        public bool IsReadOnly => false;
+
+        public void Add(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Clear()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Contains(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int IndexOf(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(int index, T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Remove(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveAt(int index)
         {
             throw new NotImplementedException();
         }
 
         public IEnumerator<T> GetEnumerator()
         {
-            return new SingleLinkedListEnumerator(this.Head);
+            return new SingleLinkedListEnumerator(_head);
         }
 
         IEnumerator IEnumerable.GetEnumerator()
         {
             return this.GetEnumerator();
+        }
+
+        private SingleNode<T> ReachNode(int index)
+        {
+            var node = _head;
+            var currentIndex = 0;
+
+            while (node != null && currentIndex < index)
+            {
+                node = node.Next;
+                currentIndex++;
+            }
+
+            if (node == null)
+            {
+                throw new IndexOutOfRangeException();
+            }
+
+            return node;
         }
 
         private class SingleLinkedListEnumerator : IEnumerator<T>
