ad9775e8202d27dfc7f6dc8351a572661c40edb3
Metgy Alexandre <alexandre.metgy@gmail.com>
2019-10-14 23:26:41 +0200

    Unit test properties on empty list.

--- DataStructures.UnitTests/SingleLinkedList_Tests.cs
+++ DataStructures.UnitTests/SingleLinkedList_Tests.cs
@@ -1,10 +1,31 @@
 using Xunit;
 using FluentAssertions;
+using System;
 
 namespace DataStructures.UnitTests
 {
     public class SingleLinkedList_Tests
     {
+        [Fact]
+        public void EmptyList_ShouldHaveCountOfZero()
+        {
+            var list = new SingleLinkedList<int>();
+
+            list.Count.Should().Be(0);
+        }
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(1)]
+        public void EmptyList_ShouldThrow_WhenIndexingOutOfRange(int index)
+        {
+            var list = new SingleLinkedList<int>();
+
+            Func<int> indexing = () => list[index];
+
+            indexing.Should().Throw<IndexOutOfRangeException>();
+        }
+
         [Fact]
         public void Add_ShouldAppendElementAtTheEndOfTheList()
         {
