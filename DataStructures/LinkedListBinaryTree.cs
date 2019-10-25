7ba1701b251b7c379f03097ca5889ea612071209
Metgy Alexandre <alexandre.metgy@gmail.com>
2019-10-25 22:25:08 +0200

    Create ArrayBinaryTree and LinkedListBinaryTree implementations of IBinaryTree.

---
+++ DataStructures/LinkedListBinaryTree.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using DataStructures.Interfaces;
+
+namespace DataStructures
+{
+    public class LinkedListBinaryTree<T> : IBinaryTree<T>
+    {
+        public void Add(T value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IBinaryTree<T> Copy()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void InOrder(Action<T> action)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void PostOrder(Action<T> action)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void PreOrder(Action<T> action)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(T value)
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
