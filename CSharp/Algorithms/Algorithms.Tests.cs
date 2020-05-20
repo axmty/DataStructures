9bb896d60566f4d74a03ef05590df1aba7d3a4da
axmty <alexandre.metgy@gmail.com>
2020-05-20 19:41:55 +0200

    Add AreAnagrams.

---
+++ CSharp/Algorithms/Algorithms.Tests.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace Algorithms
+{
+    public partial class Algorithms
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
--- CSharp/Algorithms/Algorithms.cs
+++ CSharp/Algorithms/Algorithms.cs
@@ -1,13 +1,36 @@
-using Xunit;
+using System.Collections.Generic;
 
 namespace Algorithms
 {
-    public class Algorithms
+    public partial class Algorithms
     {
-        [Fact]
-        public void Test1()
+        // Returns true if and only if the two strings are anagrams.
+        // O(1) space - O(n) time
+        public static bool AreAnagrams(string first, string second)
         {
+            if (first.Length != second.Length)
+            {
+                return false;
+            }
 
+            var lookup = new Dictionary<char, int>();
+
+            foreach (var letter in first)
+            {
+                lookup[letter] = lookup.ContainsKey(letter) ? lookup[letter] + 1 : 1;
+            }
+
+            foreach (var letter in second)
+            {
+                if (!lookup.TryGetValue(letter, out var count) || count == 0)
+                {
+                    return false;
+                }
+
+                lookup[letter] = count - 1;
+            }
+
+            return true;
         }
     }
 }
